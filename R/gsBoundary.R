c2afcc06788f308081f7c4b8e5a58a2cb9da4cb0
Eric Zhang <zhangp@eikontx.com>
2025-03-27 12:35:15 -0400

    Add HSD spedning function, and also allow specifying futility lower bound under null hypothesis

--- R/gsBoundary.R
+++ R/gsBoundary.R
@@ -1,17 +1,19 @@
 #' @title Calculate efficacy boundary for inverse normal combination test
-#' @description For an intersection hypothesis, calculate the efficacy boundary at current stage for the inverse normal combination test. Currently, the function use OBF alpha spending function to allocate alpha across stages
+#' @description For an intersection hypothesis, calculate the efficacy boundary at current stage for the inverse normal combination test. Currently, the function use an alpha spending function to allocate alpha across stages
 #' @param w A matrix of pre-specified weights for the incremental test statistics up to current stage based on cohort 1. Each row contains the weights used at the corresponding stage
 #' @param h A matrix of pre-specified weights for cohort 1 and cohort 2 up to current stage.
 #' @param d A vector of observed number of events from both cohorts across stages (up to current stage)
 #' @param d2 A vector of observed number of events from cohort 2 across stages (up to current stage)
 #' @param s The planned total number of stages for efficacy testing (excluding dose selection stage)
 #' @param planD The planned total number of events from cohort 2 at FA.
-#' @param alpha: The assigned alpha level
+#' @param alpha The assigned alpha level
+#' @param sf A spending function. 'OF' for O'Brien-Fleming spending function. 'HSD' for Hwang-Shih-DeCani spending function
+#' @param sfpar It specifies the parameter for Hwang-Shih-DeCani spending function . It will be ignored if sfu='OBF'
+#' @param bt Bound type. bt='upper' for efficacy upper bound. bt='lower' for efficacy lower bound.
 #' @import mvtnorm
 #' @importFrom gsDesign sfLDOF
 #' @return The efficacy boundaries up to current stage
 #' @export
-#'
 #' @examples
 #' # 3 doses at dose selection followed with 4 stages for the selected dose
 #' # calculate boundary at 1st stage
@@ -23,7 +25,7 @@
 #' s <- 4
 #' planD <- 520
 #' alpha <- 0.025
-#' gsBoundary(w,h,d,d2,s,planD,alpha)
+#' gsBoundary(w,h,d,d2,s,planD,alpha,sf='OF',bt='upper')
 #'
 #' # calculate boundary at 2nd stage
 #' w <- matrix(c(1,        0,
@@ -36,7 +38,7 @@
 #' s <- 4
 #' planD <- 520
 #' alpha <- 0.025
-#' gsBoundary(w,h,d,d2,s,planD,alpha)
+#' gsBoundary(w,h,d,d2,s,planD,alpha,sf='OF',bt='upper')
 #'
 #' # calculate boundary at 3rd stage
 #' w <- matrix(c(1,        0,         0,
@@ -51,7 +53,7 @@
 #' s <- 4
 #' planD <- 520
 #' alpha <- 0.025
-#' gsBoundary(w,h,d,d2,s,planD,alpha)
+#' gsBoundary(w,h,d,d2,s,planD,alpha,sf='OF',bt='upper')
 #'
 #' # calculate boundary at 4th stage
 #' w <- matrix(c(1,        0,         0,         0,
@@ -68,27 +70,54 @@
 #' s <- 4
 #' planD <- 520
 #' alpha <- 0.025
-#' gsBoundary(w,h,d,d2,s,planD,alpha)
-
-
-gsBoundary <- function(w,h,d,d2,s,planD,alpha){
+#' gsBoundary(w,h,d,d2,s,planD,alpha,sf='OF',bt='upper')
+#'
+#' # Add futility bounds
+#' gsBoundary(w,h,d,d2,s,planD,alpha=1-alpha,sf='HSD',sfpar=-6,bt='lower')
+#'
+gsBoundary <- function(w,h,d,d2,s,planD,alpha,sf,sfpar=NULL,bt){
   sc <- length(d)
   b.lst <- rep(NA,sc)
 
   if(s==1){
-    b.lst <- qnorm(alpha, lower.tail = FALSE)
+    if(bt=='upper'){
+      b.lst <- qnorm(alpha, lower.tail = FALSE)
+    } else if(bt=='lower'){
+      b.lst <- qnorm(alpha, lower.tail = TRUE)
+    }
+
   } else {
     for(ss in 1:sc){
 
       if(ss==1){
-        alpha.spent <- sfLDOF(alpha, t=c( d,planD)/planD )$spend[1]
-        b.lst[ss] <- qnorm(alpha.spent,lower.tail = F)
+        if(sf=='OF'){
+          alpha.spent <- sfLDOF(alpha, t=c( d,planD)/planD )$spend[1]
+        } else if(sf=='HSD'){
+          alpha.spent <- sfHSD(alpha, t=c( d,planD)/planD, sfpar)$spend[1]
+        }
+
+        if(bt=='upper'){
+          b.lst[ss] <- qnorm(alpha.spent,lower.tail = FALSE)
+        } else if(bt=='lower'){
+          b.lst[ss] <- qnorm(alpha.spent,lower.tail = TRUE)
+        }
+
       } else {
         if(ss==s){
-          alpha.cum <- sfLDOF(alpha, t=c( d[1:(ss-1)],planD)/planD )$spend
+          if(sf=='OF'){
+            alpha.cum <- sfLDOF(alpha, t=c( d[1:(ss-1)],planD)/planD )$spend
+          } else if(sf=='HSD'){
+            alpha.cum <- sfHSD(alpha, t=c( d[1:(ss-1)],planD)/planD, sfpar)$spend
+          }
+
           alpha.spent <- alpha.cum[ss]-alpha.cum[(ss-1)]
+
         } else {
-          alpha.cum <- sfLDOF(alpha, t=c( d,planD)/planD )$spend
+          if(sf=='OF'){
+            alpha.cum <- sfLDOF(alpha, t=c( d,planD)/planD )$spend
+          } else if(sf=='HSD'){
+            alpha.cum <- sfHSD(alpha, t=c( d,planD)/planD, sfpar )$spend
+          }
           alpha.spent <- alpha.cum[ss]-alpha.cum[(ss-1)]
         }
 
@@ -112,12 +141,19 @@ gsBoundary <- function(w,h,d,d2,s,planD,alpha){
           }
         }
 
-        # function to calculate Pr(Zi>ci, Z1<c1,Z2<c2...Zi-1<ci-1)
-        inc.prob <- function(x){
-          alpha.spent-mvtnorm::pmvnorm(mean=rep(0,ss ), corr=cov.m, lower=c(rep(-Inf,(ss-1)),x ), upper=c(b.lst[!is.na(b.lst)],Inf)   )[1]
+        if(bt=='upper'){
+          # function to calculate Pr(Zi>ci, Z1<c1,Z2<c2...Zi-1<ci-1)
+          inc.prob <- function(x){
+            alpha.spent-mvtnorm::pmvnorm(mean=rep(0,ss ), corr=cov.m, lower=c(rep(-Inf,(ss-1)),x ), upper=c(b.lst[!is.na(b.lst)],Inf)   )[1]
+          }
+        } else if(bt=='lower'){
+          # function to calculate Pr(Zi<=ci, Z1>c1,Z2>c2...Zi-1>ci-1)
+          inc.prob <- function(x){
+            alpha.spent-mvtnorm::pmvnorm(mean=rep(0,ss ), corr=cov.m, lower=c(b.lst[!is.na(b.lst)],-Inf ), upper=c(rep(Inf,(ss-1)),x)   )[1]
+          }
         }
 
-        b.lst[ss] <- uniroot(inc.prob, interval = c(0, 5))$root
+        b.lst[ss] <- uniroot(inc.prob, interval = c(-5, 5))$root
       }
 
     }
--- R/gsDose-package.R
+++ R/gsDose-package.R
@@ -2,6 +2,9 @@
 "_PACKAGE"
 
 ## usethis namespace: start
+#' @importFrom gsDesign sfHSD
 #' @importFrom gsDesign sfLDOF
+#' @importFrom stats qnorm
+#' @importFrom stats uniroot
 ## usethis namespace: end
 NULL
